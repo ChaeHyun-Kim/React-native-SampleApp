d9a9baafe05db2c1c41833d1ce8ed38c37d0e3e7
suji <csj8073@gmail.com>
2021-09-21 01:47:11 +0900

    0921

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //App.js
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, ImageBackground, ScrollView } from 'react-native';
 
 
 export default class App extends React.Component {
@@ -8,14 +8,27 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <ImageBackground
+          style={{width: '100%', height: '100%'}}
+          source={require('./icon/background.png')}>
         <View style={styles.settingView}>
+          <TouchableOpacity>
+            <Image source={require('./icon/setting.png')}/>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.ddayView}>
+            <Text style={styles.titleText}>
+              수능까지
+            </Text>
+            <Text style={styles.ddayText}>
+              D-123
+            </Text>
+            <Text style={styles.dateText}>
+              2021년 09월 21일
+            </Text>
 
-          <Text style={styles.lodaingText}>
-           안녕 채현아~~~ app 만들기 성공!
-          </Text>
-
-        </View>
-        <View style={styles.ddayView}>
+        
+        
           <Image
               style={styles.imagesize}
               source={{uri: 'https://facebook.github.io/react-native/img/tiny_logo.png'}}
@@ -23,10 +36,19 @@ export default class App extends React.Component {
 
         </View>
         <View style={styles.chatView}>
+          <ScrollView style={styles.chatScrollView}>
 
+          </ScrollView>
+          <View style={styles.chatControl}>
+            <TextInput style={styles.chatInput}/>
+            <TouchableOpacity style={styles.sendButton}>
+              <Text> 전송 </Text>
+            </TouchableOpacity>
+          </View>
         </View>
+        </ImageBackground>
       </View>
-
+    
     );
   }
 }
@@ -37,15 +59,17 @@ const styles = StyleSheet.create({
     },
     settingView: {
       flex: 1,
-      backgroundColor: 'white',
+      justifyContent: 'center',
+      alignItems: 'flex-end',
+      marginRight: '1%'
     },
     ddayView: {
       flex: 7,
-      backgroundColor: 'green',
+      justifyContent: 'center',
+      alignItems: 'center'
     },
     chatView: {
-      flex: 7,
-      backgroundColor: 'blue',
+      flex: 6,
     },
     lodaingText: {
       fontSize : 25,
@@ -53,5 +77,60 @@ const styles = StyleSheet.create({
     imagesize : {
       width : 40,
       height : 40,
+    },
+    titleText:{
+      alignSelf: 'flex-end',
+      fontSize: 36,
+      fontWeight: 'bold',
+      color: '#4A4A4A',
+      marginRight: '15%'
+    },
+    ddayText: {
+      fontSize: 100,
+      fontWeight: 'bold',
+      color: '#4A4A4A',
+  
+    },
+    dateText: {
+      alignSelf: 'flex-start',
+      fontSize: 21,
+      fontWeight: 'bold',
+      color: '#4A4A4A',
+      marginLeft: '15%'
+    },
+    sendButton:{
+      backgroundColor: 'yellow',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width : 50,
+      height : 40,
+      padding: 5,
+      marginLeft: 5,
+      borderRadius: 20,
+
+    },
+    chatInput: {
+      backgroundColor: 'white',
+      width: '75%',
+      height: 40,
+      borderWidth: 1,
+      borderColor: '#a5a5a5',
+      borderRadius: 20
+      
+    },
+    chatControl: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginBottom: 10,
+    },
+    chatScrollView: {
+      width: '90%',
+      margin: 10,
+      alignSelf: 'center',
+      backgroundColor: 'rgba(201, 201, 201, 0.7)',
+      borderRadius: 5,
+      borderWidth: 1,
+      borderColor : '#a5a5a5',
     }
 });
